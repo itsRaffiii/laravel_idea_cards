64d93a811cdf19b424f6941b82c7b6e1c883225a
Rafael Matro <matrorafael27@gmail.com>
2026-04-17 10:59:33 +0800

    New update about HTTPR and REST

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
-   
+
       <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <style>
         .max-w-1000 {
@@ -21,13 +21,13 @@
         }
     </style>
 
-    
+
 </head>
 
 <body class="bg-gray-700 p-6 max-w-xl mx-auto" >
 
     <nav style="color: aliceblue;">
-        <a href="/">HomePage</a>
+        <a href="/ideas">HomePage</a>
         <a href="/about">About Us</a>
         <a href="/contact">Contact Us</a>
     </nav>
---
+++ resources/views/ideas/show.blade.php
@@ -0,0 +1,21 @@
+<x-layout>
+
+    </br>
+    <h1 style="text-align: center; color: aliceblue;">ShowPage</h1>
+
+        <div class="mt-6 text-white">
+            <h2 class="font-bold">Your Idea</h2>
+
+                <div class="mt-6">
+                    {{$idea -> description}}
+                </div>
+
+                <div class="mt-6">
+                    <a href="/ideas/{{$idea->id}}/edit"
+                       class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+                            Edit
+                    </a>
+                </div>
+
+        </div>
+</x-layout>
