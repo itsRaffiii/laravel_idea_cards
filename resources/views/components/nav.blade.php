45576d8bffc0b1176f641ced78311e9d0e38ba4a
Rafael Matro <matrorafael27@gmail.com>
2026-05-04 11:12:12 +0800

    fixed features

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -9,16 +9,20 @@
                 class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                 <li><a>Home</a></li>
                 <li><a>New Idea</a></li>
-                <li><a>About Us</a></li>
+                @can('view-admin')
+                    <li><a href="/admin">Admin</a></li>
+                @endcan
             </ul>
         </div>
-        <a class="btn btn-ghost text-xl">Idea</a>
+        <a class="btn btn-ghost text-xl" href="/ideas">Idea</a>
     </div>
     <div class="navbar-center hidden lg:flex">
         <ul class="menu menu-horizontal px-1">
             <li><a href="/ideas">Home</a></li>
             <li><a href="/ideas/create">New Idea</a></li>
-            <li><a>About Us</a></li>
+            @can('view-admin')
+                <li><a href="/admin">Admin</a></li>
+            @endcan
         </ul>
     </div>
 
--- routes/web.php
+++ routes/web.php
@@ -4,32 +4,34 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\SessionsController;
 use App\Http\Controllers\IdeaController;
 use Illuminate\Support\Facades\Route;
-//for index
-Route::get('/ideas', [IdeaController::class, 'index']);
 
-//create
-Route::get('/ideas/create', [IdeaController::class, 'create']);
+Route::middleware('auth')->group(function () {
+    //for index
+        Route::get('/ideas', [IdeaController::class, 'index']);
+    //create
+        Route::get('/ideas/create', [IdeaController::class, 'create']);
+    //store
+        Route::post('/ideas', [IdeaController::class, 'store']);
+    //show
+        Route::get('/ideas/{idea}', [IdeaController::class, 'show']);
+    //edit
+        Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit']);
+    //update
+        Route::patch('/ideas/{idea}', [IdeaController::class, 'update']);
+    //destroy
+        Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy']);
 
-//store
-Route::post('/ideas', [IdeaController::class, 'store']);
+    Route::delete('/logout', [SessionsController::class, 'destroy']);
 
-//show
-Route::get('/ideas/{idea}', [IdeaController::class, 'show']);
+});
 
-//edit
-Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit']);
+Route::middleware('guest')->group(function () {
+    //register
+        Route::get('/register', [RegisteredUserController::class, 'create']);
+        Route::post('/register', [RegisteredUserController::class, 'store']);
 
-//update
-Route::patch('/ideas/{idea}', [IdeaController::class, 'update']);
-
-//destroy
-Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy']);
-
-//register
-Route::get('/register', [RegisteredUserController::class, 'create']);
-Route::post('/register', [RegisteredUserController::class, 'store']);
-
-Route::get('/login', [SessionsController::class, 'create']);
-Route::post('/login', [SessionsController::class, 'store']);
-Route::delete('/logout', [SessionsController::class, 'destroy']);
+    // login
+        Route::get('/login', [SessionsController::class, 'create'])->name('login');
+        Route::post('/login', [SessionsController::class, 'store']);
+});
 
